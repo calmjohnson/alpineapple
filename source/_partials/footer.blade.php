159e0c58dd2561bd208cc1ae01454b112b556ff1
Nonso Mbah <nonsombah98@gmail.com>
2022-04-29 13:39:39 +0100

    started working on the combobox component

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="my-5 flex justify-center">
-    <a class="text-white" href="https://www.flaticon.com/free-icons/pineapple" title="pineapple icons">
+    <a class="text-sm text-white" href="https://www.flaticon.com/free-icons/pineapple" title="pineapple icons">
         Pineapple icons created by Freepik - Flaticon
     </a>
 </div>
