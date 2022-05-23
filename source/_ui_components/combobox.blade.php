a4f352a207664d8a23ce527b154de5d0dbaa7045
Nonso Mbah <nonsombah98@gmail.com>
2022-05-23 22:22:52 +0100

    continued work on the combobox component

--- source/_ui_components/combobox.blade.php
+++ source/_ui_components/combobox.blade.php
@@ -46,33 +46,15 @@ image: combobox
                 <!--END: Tabs -->
                 <!--BEGIN: Preview -->
                 <div x-show="tab === 'preview'" x-ref="code" class="px-2 h-96 rounded-lg bg-gradient-to-r from-red-600 to-orange-700">
-                    <div x-data="{ selected : 1 }" class="flex flex-col justify-center items-center px-2 py-20 md:p-20 space-y-1 text-slate-800">
-                        <!--Question-->
-                        <div class="flex flex-col w-full space-y-1">
-                            <button @click="selected !== 1 ? selected = 1 : selected = null" class="flex items-center w-full bg-white p-3 rounded-md">
-                                <span class="mr-auto text-sm">First question</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" 
-                                    :class="selected === 1 ? 'rotate-180' : ''"
-                                    class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 0 1 1.414 0L10 10.586l3.293-3.293a1 1 0 1 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 0-1.414z" clip-rule="evenodd"/></svg>
-                            </button>
-                            <div x-show="selected === 1" class="p-2 text-sm rounded bg-white">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod risus sit amet dolor luctus rutrum. Proin et condimentum est. Duis ac pulvinar magna, quis tincidunt eros.
+                    <div x-data="{ selected : '' }" class="flex justify-center items-center px-2 py-20 md:px-52 space-y-1 text-slate-800">
+                        <div class="w-full relative mt-1">
+                            <div class="flex items-center cursor-default shadow bg-white rounded-lg">
+                                <input class="w-full h-10 px-3 border-none text-sm rounded-lg focus:ring-0 outline-none" type="text">
+                                <button class="px-1 h-auto">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-5 w-5 text-gray-400"><path fill-rule="evenodd" d="M10 3a1 1 0 0 1 .707.293l3 3a1 1 0 0 1-1.414 1.414L10 5.414 7.707 7.707a1 1 0 0 1-1.414-1.414l3-3A1 1 0 0 1 10 3zm-3.707 9.293a1 1 0 0 1 1.414 0L10 14.586l2.293-2.293a1 1 0 0 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z" clip-rule="evenodd"/></svg>
+                                </button>
                             </div>
                         </div>
-                        <!--Question-->
-                        <!--Question-->
-                        <div class="flex flex-col w-full space-y-1">
-                            <button @click="selected !== 2 ? selected = 2 : selected = null" class="flex items-center w-full bg-white p-3 rounded-md">
-                                <span class="mr-auto text-sm">Second question</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" 
-                                    :class="selected === 2 ? 'rotate-180' : ''"
-                                    class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 0 1 1.414 0L10 10.586l3.293-3.293a1 1 0 1 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 0-1.414z" clip-rule="evenodd"/></svg>
-                            </button>
-                            <div x-show="selected === 2" class="p-2 text-sm rounded bg-white">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod risus sit amet dolor luctus rutrum. Proin et condimentum est. Duis ac pulvinar magna, quis tincidunt eros.
-                            </div>
-                        </div>
-                        <!--Question-->
                     </div>
                 </div>
                 <!--END: Preview -->
