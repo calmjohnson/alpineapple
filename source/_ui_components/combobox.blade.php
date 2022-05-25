4b0a1a98aa1f2c616399dba911649d42686c9ba0
Nonso Mbah <nonsombah98@gmail.com>
2022-05-25 20:30:06 +0100

    more work on the combobox component

--- source/_ui_components/combobox.blade.php
+++ source/_ui_components/combobox.blade.php
@@ -46,15 +46,34 @@ image: combobox
                 <!--END: Tabs -->
                 <!--BEGIN: Preview -->
                 <div x-show="tab === 'preview'" x-ref="code" class="px-2 h-96 rounded-lg bg-gradient-to-r from-red-600 to-orange-700">
-                    <div x-data="{ selected : '' }" class="flex justify-center items-center px-2 py-20 md:px-52 space-y-1 text-slate-800">
+                    <div x-data="{ selected : '', open : false }" class="flex flex-col justify-center items-center px-2 py-20 md:px-52 space-y-1 text-slate-800">
                         <div class="w-full relative mt-1">
                             <div class="flex items-center cursor-default shadow bg-white rounded-lg">
-                                <input class="w-full h-10 px-3 border-none text-sm rounded-lg focus:ring-0 outline-none" type="text">
-                                <button class="px-1 h-auto">
+                                <input x-model="selected" @input="open = true" class="w-full h-10 px-3 border-none text-sm rounded-lg focus:ring-0 outline-none" type="text">
+                                <button @click="open = ! open" class="px-1 h-auto">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-5 w-5 text-gray-400"><path fill-rule="evenodd" d="M10 3a1 1 0 0 1 .707.293l3 3a1 1 0 0 1-1.414 1.414L10 5.414 7.707 7.707a1 1 0 0 1-1.414-1.414l3-3A1 1 0 0 1 10 3zm-3.707 9.293a1 1 0 0 1 1.414 0L10 14.586l2.293-2.293a1 1 0 0 1 1.414 1.414l-3 3a1 1 0 0 1-1.414 0l-3-3a1 1 0 0 1 0-1.414z" clip-rule="evenodd"/></svg>
                                 </button>
                             </div>
                         </div>
+                        <pre><x-torchlight-code language='php'>
+
+                            echo "hello world";
+                        
+                        </x-torchlight-code></pre>
+                        <!--Options-->
+                        <div x-show="open === true" @click.outside="open = false" class="flex flex-col justify-start w-full bg-white list-none px-5 py-2 rounded-lg shadow-md">
+                            <template>
+                                <div>Nothing found...</div>
+                            </template>
+                            
+                            <template x-for="framework in frameworks" :key="framework.id">
+                                <li @click="selected = framework.name" tabindex="0" class="relative cursor-default focus:text-red-900 select-none py-1 pr-4">
+                                    <span class="blcok truncate" x-text="framework.name"></span>
+                                </li>
+                            </template>
+                            
+                        </div>
+                        <!--Options-->
                     </div>
                     <!--JS-->
                     <script>
@@ -66,21 +85,6 @@ image: combobox
                             { id: 5, name: 'Svelte js'},
                             { id: 6, name: 'Statamic'}
                         ]
-
-                        let selected = ref(frameworks[0]);
-
-                        let query = ref('');
-
-                        let filteredFrameworks = computed(() =>
-                        query.value === ''
-                            ? frameworks
-                            : frameworks.filter((framework) =>
-                                framework.name
-                                .toLowerCase()
-                                .replace(/\s+/g, '')
-                                .includes(query.value.toLowerCase().replace(/\s+/g, ''))
-                            )
-                        )
                     </script>
                     <!--JS-->
                 </div>
